a9d9c67e883eaba4c92fb8cf359e41e8c78877c6
Brian Quiroz <r229q057@ku.edu>
2019-03-24 04:17:05 -0500

    improved algorithm, will comment code later and explain

--- HMM.py
+++ HMM.py
@@ -8,9 +8,13 @@ import random
 import string
 
 firstWords = {}
-secondWords = {}
-thirdWords = {}
+pairs = {}
+triples = {}
 transitions = {}
+
+NUMBER_OF_LINES = 30
+MAX_NUMBER_OF_WORDS_PER_LINE = 20
+end = "tokEND"
 
 def tokenize(row):
     #cutting into words and removing punctuation
@@ -37,9 +41,40 @@ def listToDict(dict, word):
     for num in list:
         updateValueInDict(subDict, num)
     normalize(subDict, listLength)
+    return subDict
 
-def getRandomValueFromDict(dict):
-    return random.choice(list(set(dict)))
+def getRandomValueFromDict(dict, key):
+    if dict == firstWords:
+        return random.choice(list(set(dict)))
+    else:
+        if key in dict:
+            subDict = dict[key]
+            if len(subDict) > 1:
+                return random.choice(list(set(subDict)))
+            else:
+                if dict == transitions:
+                    attempt1 = getRandomValueFromDict(triples, (key[1],key[2]))
+                    if attempt1 == end:
+                        attempt2 = getRandomValueFromDict(pairs, key[1])
+                        if attempt2 == end:
+                            return random.choice(list(set(subDict)))
+                        else:
+                            return attempt2
+                    else:
+                        return attempt1
+
+                elif dict == triples:
+                    attempt = getRandomValueFromDict(pairs, key[1])
+                    if attempt == end:
+                        return random.choice(list(set(subDict)))
+                    else:
+                        return attempt
+
+                else:
+                    return random.choice(list(set(subDict)))
+        else:
+            # print(key,"NOT FOUND")
+            return end
 
 def readFromFile():
     ##read lines from file
@@ -64,55 +99,82 @@ def train(rows):
 
             else:
                 if i == 1:
-                    insertValue(secondWords, tokens[i - 1], token)
+                    insertValue(pairs, tokens[i - 1], token)
                 elif i == 2:
-                    insertValue(thirdWords, (tokens[i - 2], tokens[i - 1]), token)
+                    insertValue(triples, (tokens[i - 2], tokens[i - 1]), token)
                 else:
                     insertValue(transitions, (tokens[i - 3], tokens[i - 2], tokens[i - 1]), token)
 
                 if i == amountOfTokens - 1 and amountOfTokens > 1:
-                    insertValue(transitions, (tokens[i - 2], tokens[i - 1], token), 'tokEND')
+                    insertValue(transitions, (tokens[i - 2], tokens[i - 1], token), end)
 
     firstWordsSum = sum(firstWords.values())
     normalize(firstWords, firstWordsSum)
 
-    for word in secondWords.keys():
-        listToDict(secondWords, word)
+    for word in pairs.keys():
+        pairs[word] = listToDict(pairs, word)
 
-    for word in thirdWords.keys():
-        listToDict(thirdWords, word)
+    for word in triples.keys():
+        triples[word] = listToDict(triples, word)
 
     for word in transitions.keys():
-        listToDict(transitions, word)
+        transitions[word] = listToDict(transitions, word)
+
+    # print(firstWords)
+    # print(pairs)
+    # print(triples)
+    # print(transitions)
 
 
 def generateLine():
     line = []
-    first = getRandomValueFromDict(firstWords)
+    first = getRandomValueFromDict(firstWords,"")
     line.append(first)
-    if first in secondWords.keys():
-        second = getRandomValueFromDict(secondWords[first])
+
+    second = getRandomValueFromDict(pairs, first)
+    third = end
+    if not second == end:
         line.append(second)
-        if (first, second) in thirdWords.keys():
-            third = getRandomValueFromDict(thirdWords[(first, second)])
+        third = getRandomValueFromDict(triples, (first, second))
+        if not third == end:
             line.append(third)
         else:
-            third = "tokEND"
+            third = getRandomValueFromDict(pairs, second)
+            if not third == end:
+                # print("Third worked with pairs!")
+                line.append(third)
+            else:
+                third = end
     else:
-        second = "tokEND"
+        second = end
 
-    if (not third == "tokEND" and not second == "tokEND"):
+    if (not third == end and not second == end):
         next = ""
-        while(not next == "tokEND"):
-            if (first, second, third) in transitions.keys():
-                next = getRandomValueFromDict(transitions[(first, second, third)])
-                if (not next == "tokEND"):
+        while(not next == end and len(line) <= MAX_NUMBER_OF_WORDS_PER_LINE):
+            next = getRandomValueFromDict(transitions,(first, second, third))
+            if not next == end:
+                line.append(next)
+                first = second
+                second = third
+                third = next
+            else:
+                next = getRandomValueFromDict(triples, (second, third))
+                if not next == end:
+                    # print("Next worked with triples!")
                     line.append(next)
                     first = second
                     second = third
                     third = next
-            else:
-                next = "tokEND"
+                else:
+                    next = getRandomValueFromDict(pairs, third)
+                    if not next == end:
+                        # print("Next worked with pairs!")
+                        line.append(next)
+                        first = second
+                        second = third
+                        third = next
+                    else:
+                        next = end
 
     return line
 
@@ -120,10 +182,14 @@ def main():
     data = readFromFile()
     print("Training...")
     train(data)
-    numberOfPlays = 30
-    for i in range(0, numberOfPlays):
+    play = []
+    for i in range(0, NUMBER_OF_LINES):
         line = generateLine()
-        print(' '.join(line))
+        play.append(' '.join(line))
+
+    print("Play:",'\n')
+    for line in play:
+        print(line,'\n')
 
 if __name__== "__main__":
   main()
