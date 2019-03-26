de7bff07a31aced72e246d2f603e3dc3e4de6a78
Brian Quiroz <r229q057@ku.edu>
2019-03-25 23:53:02 -0500

    final commit

--- HMM.py
+++ HMM.py
@@ -43,37 +43,6 @@ def listToDict(dict, word):
     normalize(subDict, listLength)
     return subDict
 
-def getRandomValueFromDict(dict, key):
-    if dict == firstWords:
-        return random.choice(list(set(dict)))
-    else:
-        if key in dict:
-            subDict = dict[key]
-            if len(subDict) > 1:
-                return random.choice(list(set(subDict)))
-            else:
-                if dict == thirdOrder:
-                    attempt1 = getRandomValueFromDict(secondOrder, (key[1],key[2]))
-                    if attempt1 == end:
-                        attempt2 = getRandomValueFromDict(firstOrder, key[1])
-                        if attempt2 == end:
-                            return random.choice(list(set(subDict)))
-                        else:
-                            return attempt2
-                    else:
-                        return attempt1
-
-                elif dict == secondOrder:
-                    attempt = getRandomValueFromDict(firstOrder, key[1])
-                    if attempt == end:
-                        return random.choice(list(set(subDict)))
-                    else:
-                        return attempt
-
-                else:
-                    return random.choice(list(set(subDict)))
-        else:
-            return end
 
 def readFromFile():
     ##read lines from file
@@ -83,6 +52,7 @@ def readFromFile():
 
 
 def train(rows):
+    print("Prithee waiteth while our alg'rithm is being train'd on thy dataset...")
     for i in range(0,len(rows)):
         original = rows[i]
         tokens = tokenize(original)
@@ -91,57 +61,155 @@ def train(rows):
         for i in range(0, amountOfTokens):
             token = tokens[i]
             if i == 0:
+                #We map the words that occur at the beginning of each line to
+                # the amount of times they occur in the text (as initial words)
                 updateValueInDict(firstWords, token)
 
             else:
                 if i == 1:
+                    #Inserting into first order Markov chain.
                     insertValue(firstOrder, tokens[i - 1], token)
                 elif i == 2:
+                    #Inserting into first order Markov chain.
+                    insertValue(firstOrder, tokens[i - 1], token)
+                    #Inserting into second order Markov chain.
                     insertValue(secondOrder, (tokens[i - 2], tokens[i - 1]), token)
                 else:
+                    #Inserting into first order Markov chain.
+                    insertValue(firstOrder, tokens[i - 1], token)
+                    #Inserting into second order Markov chain.
+                    insertValue(secondOrder, (tokens[i - 2], tokens[i - 1]), token)
+                    #Inserting into third order Markov chain.
                     insertValue(thirdOrder, (tokens[i - 3], tokens[i - 2], tokens[i - 1]), token)
 
                 if i == amountOfTokens - 1 and amountOfTokens > 1:
+                    #Inserting into first order Markov chain.
+                    insertValue(firstOrder, token, end)
+                    #Inserting into second order Markov chain.
+                    insertValue(secondOrder, (tokens[i - 1], token), end)
+                    #Inserting into third order Markov chain.
                     insertValue(thirdOrder, (tokens[i - 2], tokens[i - 1], token), end)
 
+    #We normalize the dictionary of first words.
     firstWordsSum = sum(firstWords.values())
     normalize(firstWords, firstWordsSum)
 
+    #We normalize the dict corresponding to the first order Markov chain.
     for word in firstOrder.keys():
         firstOrder[word] = listToDict(firstOrder, word)
 
+    #We normalize the dict corresponding to the second order Markov chain.
     for word in secondOrder.keys():
         secondOrder[word] = listToDict(secondOrder, word)
 
+    #We normalize the dict corresponding to the third order Markov chain.
     for word in thirdOrder.keys():
         thirdOrder[word] = listToDict(thirdOrder, word)
 
 
+def getRandomValueFromDict(dict, key):
+    #When this function is called with "firstWords", we simply return a random
+    # value from the dictionary.
+    if dict == firstWords:
+        return random.choice(list(set(dict)))
+    else:
+        if key in dict:
+            #SubDict represents the dictionary within dict that corresponds to
+            # the passed in word(s)
+            subDict = dict[key]
+
+            #If the length of the sub dictionary is 1, that means our algorithm
+            # has only identified one word that follows the word (or pair or
+            # triple). Hence, if we return that value we will be creating a
+            # string of 2-4 words that surely existed in the training set. Since
+            # we want to avoid repeating long strings of text from the training
+            # set, we will attempt to reduce the amount of times we return
+            # such a "unique" value by looking in other dictionaries that
+            # are less likely to only lead to one option (if possible).
+            if len(subDict) > 1:
+                return random.choice(list(set(subDict)))
+            else:
+                #If our original dictionary was "thirdOrder" we try
+                # "secondOrder" and possibly "firstOrder".
+                if dict == thirdOrder:
+                    #We try to use the last two words instead of the last three
+                    # to see if we can obtain more than one option.
+                    attempt1 = getRandomValueFromDict(secondOrder, (key[1],key[2]))
+                    if attempt1 == end:
+                        #We try to use the last word instead of the last two to
+                        # see if we can obtain more than one option.
+                        attempt2 = getRandomValueFromDict(firstOrder, key[1])
+                        if attempt2 == end:
+                            #If both attempts are unsuccessful, we return the
+                            # only option obtained from "thirdOrder".
+                            return random.choice(list(set(subDict)))
+                        else:
+                            #If the first attempt failed but the second one
+                            # succeeded, we return what we obtained from
+                            # that second attempt.
+                            return attempt2
+                    else:
+                        #If the first attempt succeeded, we return what we
+                        # obtained from that attempt.
+                        return attempt1
+
+                #If our original dictionary was "secondOrder", we try
+                # "firstOrder".
+                elif dict == secondOrder:
+                    #We try to use the lsat word instead of the last two words
+                    # to see if we can obtain more than one option.
+                    attempt = getRandomValueFromDict(firstOrder, key[1])
+                    if attempt == end:
+                        #If the attempt was unsuccessful, we return the only
+                        # option obtained from "secondOrder".
+                        return random.choice(list(set(subDict)))
+                    else:
+                        #If the attempt succeeded, we return what we obtained
+                        # from that attempt.
+                        return attempt
+
+                else:
+                    return random.choice(list(set(subDict)))
+        #If we can't find the value in the dictionary, we return the end token.
+        else:
+            return end
+
+
 def generateLine():
     line = []
+
+    #The first word is a random word from the set of first words obtained from
+    # the training set.
     first = getRandomValueFromDict(firstWords,"")
     line.append(first)
 
+    #We attempt to predict the second word based on the first word.
     second = getRandomValueFromDict(firstOrder, first)
     third = end
+
     if not second == end:
         line.append(second)
+        #We attempt to predict the third word based on the first two words.
         third = getRandomValueFromDict(secondOrder, (first, second))
         if not third == end:
             line.append(third)
         else:
+            #If the first attempt was unsuccessful, we attempt to predict the
+            # third word based only on the second word.
             third = getRandomValueFromDict(firstOrder, second)
             if not third == end:
-                # print("Third worked with firstOrder!")
                 line.append(third)
             else:
                 third = end
     else:
         second = end
 
+    #If we have successfully predicted the last two words, we continue predicting the next words.
     if (not third == end and not second == end):
         next = ""
+        #We will continue generating new words until we reach a dead end or until we reach the maximum number of words per line.
         while(not next == end and len(line) <= MAX_NUMBER_OF_WORDS_PER_LINE):
+            #We attempt to predict the next word based on the the previous three words.
             next = getRandomValueFromDict(thirdOrder,(first, second, third))
             if not next == end:
                 line.append(next)
@@ -149,6 +217,7 @@ def generateLine():
                 second = third
                 third = next
             else:
+                #If the first attempt was unsuccessful, we attempt to predict the next word based only on the previous two words.
                 next = getRandomValueFromDict(secondOrder, (second, third))
                 if not next == end:
                     line.append(next)
@@ -156,6 +225,7 @@ def generateLine():
                     second = third
                     third = next
                 else:
+                    #If the first and second attempts were unsuccessful, we attempt to predict the next word based only on the previous word.
                     next = getRandomValueFromDict(firstOrder, third)
                     if not next == end:
                         line.append(next)
@@ -163,22 +233,23 @@ def generateLine():
                         second = third
                         third = next
                     else:
+                        #If all three attempts were unsuccessful, we give up.
                         next = end
 
     return line
 
 def main():
     data = readFromFile()
-    print("Training...")
     train(data)
     play = []
+    print("Alloweth me to gen'rate a playeth f'r thee, mine own lief sir or mistress...")
     for i in range(0, NUMBER_OF_LINES):
         line = generateLine()
         play.append(' '.join(line))
 
-    print("Play:",'\n')
+    print("They don't call me Shakespeare for nothing:\n")
     for line in play:
-        print(line,'\n')
+        print('\t',line,'\n')
 
 if __name__== "__main__":
   main()
